4da59f91439da033a3fa09f57a647965ae7803d2
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-03-13 22:50:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// Write your code
+let multiply;
+let num1 =31;
+let num2 =2;
+multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random;
+let num7 = 0;
+random = num7 += 1;
+console.log(random);
+
+let mod;
+let num3 = 4;
+let num4 = 8;
+mod = num3 % num4;
+console.log(mod);
+
+let max;
+max = Math.max(12, 5 ,6 ,19 , 20, 17);
+console.log(max);
